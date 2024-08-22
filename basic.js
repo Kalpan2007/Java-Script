331a7547a748232d9fc9d92af7952ad89a1a1743
Kalpan2007 <kalpankaneriya@gmail.com>
2024-08-22 14:04:47 +0530

    Update basic.js

---
+++ basic.js
@@ -0,0 +1,88 @@
+//q1 1. **Print "Hello, World!"**
+//console.log("hello")
+
+//q2 2. **Add Two Numbers**  
+ //  - Task: Write a function that takes two numbers and returns their sum.
+// let a = 3
+// let b = 5
+// console.log(a + b)
+
+//q3 3. **Find the Largest Number**  
+//   - Task: Write a function that takes three numbers and returns the largest.  
+// let a = 2 
+// let b = 8 
+// let c = 3
+// function findlargest(a,b,c) {
+//    return Math.max(a,b,c);
+// }
+// console.log(findlargest(a,b,c));
+
+//q4 4. **Check if a Number is Even or Odd**  
+//   - Task: Write a function that checks if a number is even or odd.  
+// num = 100;
+// if (num%2 == 0){
+//     console.log("this num is even")
+// }else{
+//     console,log("this num is odd")
+// }
+
+//q5 5. **Calculate the Factorial of a Number**  
+//   - Task: Write a function that returns the factorial of a number.  
+    // function fac(integer) {
+    //     let factorial = 1;
+        
+    //     for(let i = 1; i <= integer; i++) {
+    //         factorial *= i; 
+    //     }
+    //     return factorial; 
+    // }
+    
+    // let number = 5;
+    // console.log(fac(number));
+
+
+//q6     6. **Reverse a String**  
+//   - Task: Write a function that takes a string and returns it reversed. 
+// function Reversestring(str){
+// return str.split('').reverse().join('');
+// }
+// console.log(Reversestring("hello"))
+
+//q7. **Check for Palindrome**  
+//   - Task: Write a function that checks if a given string is a palindrome
+// function palindrome(str){
+// return str === str.split('').reverse().join('');
+// }
+// console.log(palindrome("12321"))
+
+//Q8 8. **Find the Minimum Number in an Array**  
+//   - Task: Write a function that takes an array and returns the smallest number.
+// let a = 1
+// let b = 2
+// let c = 4 
+// function findminimum(a,b,c){
+//    return Math.min(a,b,c);
+// }
+// console.log(findminimum(a,b,c));
+
+
+// //q9    9. **Sort an Array**  
+// //   - Task: Write a function that sorts an array of numbers in ascending order. 
+// function sortarray(arr){
+//     return arr.sort((a,b) => a-b);
+// }
+// console.log(sortarray([1,3,1,4,5,]))
+
+
+
+//q10   10. **Check if a String Contains a Substring**  
+//    - Task: Write a function that checks if a string contains another substring.
+// function ContainsaSubstring(mainstring,substring){
+//     return mainstring.includes(substring);
+// }
+// console.log(ContainsaSubstring("hello word","planet"))
+
+
+
+//q11 11. **Sum All Elements in an Array**  
+- Task: Write a function that returns the sum of all numbers in an array
